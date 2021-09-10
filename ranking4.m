88d8f7083493d07a9a21ac63e0c04870e924ab08
louis-dreyer <90194140+louis-dreyer@users.noreply.github.com>
2021-09-10 12:47:24 +0200

    Add files via upload

---
+++ ranking4.m
@@ -0,0 +1,19 @@
+#Wie ranking3, nur mit veraenderbarem d
+
+function ranking4 (P, d) 
+  #nullspalten finden
+  n = columns(P);
+  for i=1:n;
+    if P(:,i) == zeros(n);
+      P(:,i) = 1/n;
+    endif
+  endfor
+  #Daempfungsfaktor
+  M = ones(n);
+  P2 = d*P+(1-d)*(1/8)*M
+  #eigentliche
+  [a, b] = eig(P2);
+  a = a(:,1:1)
+  [c, d] = sortrows (-a);
+  d
+endfunction
